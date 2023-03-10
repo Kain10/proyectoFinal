da68ab5b817fb2c0474e06f335424cd51cdfa87f
kain <abelillo28@gmail.com>
2023-03-10 21:32:01 +0100

    metodo multiplicar hecho

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -3,5 +3,9 @@ package HelloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("¡Hello world!");
+        System.out.println(multiplicar(2,3));
+    }
+    public static int multiplicar(int x , int y){
+        return x*y;
     }
 }
