176631d207865d170f676d24ee6affe70a9b095f
jrProfessor <jr.amurriomartinez@edu.gva.es>
2023-03-11 00:13:23 +0100

    metodo dividir

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -8,8 +8,13 @@ public class HelloWorld {
         System.out.println("¡Hello world!");
         System.out.println(sumar(5, 10));
         System.out.println(multiplicar(2,3));
+        System.out.println(dividir(10, 2));
     }
     public static int multiplicar(int x , int y){
         return x*y;
+    }
+
+    public static double dividir(double x, double y){
+        return x/y;
     }
 }
