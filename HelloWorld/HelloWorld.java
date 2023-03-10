cf4008f86c1d6bac80716336a37ec7d94817b76a
jrProfessor <jr.amurriomartinez@edu.gva.es>
2023-03-10 21:23:22 +0100

    metodo sumar

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,7 +1,11 @@
 package HelloWorld;
 
 public class HelloWorld {
+    public static int sumar(int x, int y){
+        return x+y;
+    }
     public static void main(String[] args) {
         System.out.println("¡Hello world!");
+        System.out.println(sumar(5, 10));
     }
 }
