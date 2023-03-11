bb79ed1d1cdb9b81ccc79a3bceb7bd4b9b82dac7
jrProfessor <jr.amurriomartinez@edu.gva.es>
2023-03-11 07:32:42 +0100

    metodo restar

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -4,11 +4,16 @@ public class HelloWorld {
     public static int sumar(int x, int y){
         return x+y;
     }
+
+    public static int restar(int x, int y){
+        return x-y;
+    }
     public static void main(String[] args) {
         System.out.println("¡Hello world!");
-        System.out.println(sumar(5, 10));
-        System.out.println(multiplicar(2,3));
-        System.out.println(dividir(10, 2));
+        System.out.println(sumar(10, 5));
+        System.out.println(multiplicar(10, 5));
+        System.out.println(dividir(10, 5));
+        System.out.println(restar(10, 5));
     }
     public static int multiplicar(int x , int y){
         return x*y;
